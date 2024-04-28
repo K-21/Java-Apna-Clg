726d5a699aa50e7b62360f54a85ccce20a6c0221
K-21 <84450912+K-21@users.noreply.github.com>
2024-04-28 19:22:06 +0530

    Update butterflyPattern.java

--- butterflyPattern.java
+++ butterflyPattern.java
@@ -24,7 +24,7 @@ public class butterflyPattern {
             for(int j=1;j<=i;j++){
                 System.out.print("*");
         }
-       // System.out.println();
+        System.out.println();
     }
         // 2nd half
         for(int i=n;i>=1;i--){
